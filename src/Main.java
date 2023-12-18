e7e587e8b0a54f96246133c6bf22ac260927deea
realdjun2 <realdjunodon@gmail.com>
2023-12-18 23:42:16 +0800

    added my answers

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+public static void main(String [] args){
+    
+    School school_name = new School("Northern Bukidnon State College");
+    school_name.NBSC();
+    
+    Programs program_name = new Programs("");
+    program_name.coursesName();
+
+	
+}
+}
---
+++ src/School.java
@@ -0,0 +1,27 @@
+public class School {
+   String name;
+
+School(String name) {
+    this.name = name;
+    
+}
+
+void NBSC(){
+    System.out.println(name + "(NBSC)");
+}
+
+void BSBA (){
+    System.out.println(name + "BSBA");
+     
+}
+
+void BSIT (){
+    System.out.println("BSIT");
+    
+}
+
+void TEP (){
+    System.out.println("TEP");
+}
+}
+
